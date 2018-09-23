f74054e22fcfb6f84f02b8b9399d9d41e8336533
門平晴樹 <kadohiraharuki@kadohiraharukinoMacBook-puro.local>
2018-09-24 00:11:28 +0900

    API(自分のアカウント)を取得できた

--- app/src/main/java/com/example/kadohiraharuki/getapi/MainActivity.kt
+++ app/src/main/java/com/example/kadohiraharuki/getapi/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.kadohiraharuki.getapi
 
 import android.database.CursorJoiner
+import android.icu.lang.UCharacter.GraphemeClusterBreak.V
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.PrecomputedText
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONException
 import org.json.JSONObject
@@ -15,6 +17,7 @@ import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
 
+//https://qiita.com/minme31/items/a9636cb0453524c64e67  (参考記事)
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,8 +26,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         btn.setOnClickListener {
             //ボタンがクリックされたらAPIを叩く。
+            HitAPITask().execute("https://api.github.com/user?access_token=d92536c1d26a108b613d3522a5893167703aa08d")
 
-            HitAPITask().execute("https://api.github.com/users/:HaruKado")
         }
 
     }
@@ -32,8 +35,11 @@ class MainActivity : AppCompatActivity() {
     inner class HitAPITask : AsyncTask<String, String, String>() {
         override fun doInBackground(vararg params: String?): String {
             //ここでAPIを叩きます。バックグラウンドで処理する内容です。
+            //HTTP固有の機能をサポートするURLConnection
             var connection: HttpURLConnection? = null
+            //文字、配列、行をバッファリングすることによって、文字型入力ストリームからテキストを効率良く読み込む
             var reader: BufferedReader? = null
+            //Stringクラス同様に宣言した変数に、文字列を格納するために使用
             val buffer: StringBuffer
 
             try {
@@ -42,6 +48,7 @@ class MainActivity : AppCompatActivity() {
                 val url = URL(params[0])
                 connection = url.openConnection() as HttpURLConnection
                 connection.connect()  //ここで指定したAPIを叩いてみてます。
+                Log.d("TAG","デバック")
 
                 //ここから叩いたAPIから帰ってきたデータを使えるよう処理していきます。
 
@@ -56,29 +63,40 @@ class MainActivity : AppCompatActivity() {
                         break
                     }
                     buffer.append(line)
-                    //Log.d("CHECK", buffer.toString())
+                    Log.d("CHECK", buffer.toString())
                 }
 
                 //ここからは、今回はJSONなので、いわゆるJsonをParseする作業（Jsonの中の一つ一つのデータを取るような感じ）をしていきます。
 
                 //先ほどbufferに入れた、取得した文字列
                 val jsonText = buffer.toString()
-
+                // .format型でデバック
+                Log.d("CHECK2", jsonText.format())
                 //JSONObjectを使って、まず全体のJSONObjectを取ります。
+
                 val parentJsonObj = JSONObject(jsonText)
-                //今回のJSONは配列になっているので（データは一つですが）、全体のJSONObjectから、getJSONArrayで配列"movies"を取ります。
-                val parentJsonArray = parentJsonObj.getJSONArray("movies")
+                Log.d("CHECK3", parentJsonObj.toString())
+
+                /*//今回のJSONは配列になっているので（データは一つですが）、全体のJSONObjectから、getJSONArrayで配列"movies"を取ります。
+                val parentJsonArray = parentJsonObj.getJSONArray("account")
+                //Log.d("CHECK4", parentJsonArray.toString())
 
                 //JSONArrayの中身を取ります。映画"Your Name"のデータは、配列"movies"の０番目のデータなので、
                 val detailJsonObj = parentJsonArray.getJSONObject(0)  //これもJSONObjectとして取得
+                */
+
 
                 //moviesの0番目のデータのtitle項目をStringで取ります。これで中身を取れました。
-                val movieName: String = detailJsonObj.getString("title")  // => Your Name.
+                //val sinceName: String = detailJsonObj.getString("login")  // => Your Name.
+                //取得したAPIのloginをsinceNameに格納
+                val sinceName: String = parentJsonObj.getString("login")
+                Log.d("CHECK4", parentJsonObj.toString())
+
                 //公開年を取りたい時も同じようにすれば良いです。
-                val year: Int = detailJsonObj.getInt("year")  // => 2016
+                //val year: Int = detailJsonObj.getInt("year")  // => 2016
 
                 //Stringでreturnしてあげましょう。
-                return "$movieName - $year"  // => Your Name. - 2016
+                return "$sinceName"  // => Your Name. - 2016
 
                 //ここから下は、接続エラーとかJSONのエラーとかで失敗した時にエラーを処理する為のものです。
             } catch (e: MalformedURLException) {
@@ -96,17 +114,23 @@ class MainActivity : AppCompatActivity() {
                 } catch (e: IOException) {
                     e.printStackTrace()
                 }
-
+            }
+                //textView.text =("失敗")
                 //失敗した時はnullやエラーコードなどを返しましょう。
                 return null.toString()
-            }
+
         }
+
+
+
 
 
         //返ってきたデータをビューに反映させる処理はonPostExecuteに書きます。これはメインスレッドです。
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-            if (result == null) return
+            if (result == null) {
+                return
+            }
 
             textView.text = result
         }
